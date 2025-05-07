2f798e433f7a17f22f2bff87d27040ca4fe6be4b
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-07 19:48:06 -0400

    section 1.21-initializing data with spring boot

---
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author();
+        Author rod = new Author();
+
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
+
+        rod.setFirstName("Rod");
+        rod.setLastName("Johnson");
+
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("978-0321125217");
+
+        Book noEJB = new Book();
+        ddd.setTitle("J2EE Development without EJB");
+        ddd.setIsbn("978-0764558313");
+
+        // good practice to assign the object returned by .save()
+        Author ericSaved = authorRepository.save(eric);
+        Author rodSaved = authorRepository.save(rod);
+
+        Book dddSaved = bookRepository.save(ddd);
+        Book noEJBSaved = bookRepository.save(noEJB);
+
+        ericSaved.getBooks().add(dddSaved);
+        rodSaved.getBooks().add(noEJBSaved);
+
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+    }
+
+
+}
